60751806c2c5337f4f5d70d29acfd946a34f4fb6
Segger87 <Segger87@gmail.com>
2018-08-06 12:50:32 +0100

    Created working solver need to refactor to make cleaner

--- SodukuSolver/SodukuSolver/Program.cs
+++ SodukuSolver/SodukuSolver/Program.cs
@@ -8,6 +8,8 @@ namespace SodukuSolver
 {
 	class Program
 	{
+		public static Tuple<int, int> GridFull = Tuple.Create(9, 9);
+
 		static void Main(string[] args)
 		{
 
@@ -23,12 +25,21 @@ namespace SodukuSolver
 				{ 0,0,9,7,0,0,0,0,0 }
 				};
 
-			new Program().PrintSudokuGrid(sudoku);
+			PrintSudokuGrid(sudoku);
+
+			var result = SolveSoduku(sudoku);
+
+			if (result == true)
+			{
+				PrintSudokuGrid(sudoku);
+			}
+
 			Console.ReadLine();
 		}
 
-		public void PrintSudokuGrid(int[,] sudokuGrid)
+		public static void PrintSudokuGrid(int[,] sudokuGrid)
 		{
+			Console.WriteLine("__________________");
 			for (int i = 0; i < 9; i++)
 			{
 				for (int j = 0; j < 9; j++)
@@ -41,13 +52,104 @@ namespace SodukuSolver
 					else
 					{
 						Console.ForegroundColor = ConsoleColor.Green;
-						Console.Write(sudokuGrid[i,j]);
+						Console.Write(sudokuGrid[i, j]);
 						Console.ResetColor();
 						Console.Write("|");
 					}
 				}
 				Console.WriteLine("");
 			}
+			Console.WriteLine();
+		}
+
+		public static bool IsTheCurrentNumberAlreadyInTheRow(int[,] sudokuGrid, int row, int num)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				if (row < 9 && sudokuGrid[row, i] == num)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static bool IsTheCurrentNumberAlreadyInTheColumn(int[,] sudokuGrid, int col, int num)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				if (col < 9 && sudokuGrid[i, col] == num)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static bool IsTheCurrentNumberInABox(int[,] sudokuGrid, int boxStartRow, int boxStartCol, int num)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					if (boxStartRow < 9 && boxStartCol < 9 && sudokuGrid[i + boxStartRow, j + boxStartCol] == num)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		public static bool IsTheMoveLegal(int[,] sudokuGrid, int row, int col, int num)
+		{
+			return !IsTheCurrentNumberAlreadyInTheRow(sudokuGrid, row, num) &&
+				   !IsTheCurrentNumberAlreadyInTheColumn(sudokuGrid, col, num) &&
+				   !IsTheCurrentNumberInABox(sudokuGrid, row - row % 3, col - col % 3, num);
+		}
+
+		public static Tuple<int, int> GetUnassignedLocations(int[,] sudokuGrid)
+		{
+			
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+				{
+					if (sudokuGrid[i, j] == 0)
+					{
+						return Tuple.Create(i, j);
+					}
+				}
+			}
+
+			return GridFull;
+		}
+
+		public static bool SolveSoduku(int[,] sudokuGrid)
+		{
+
+			if (GridFull == GetUnassignedLocations(sudokuGrid))
+			{
+				return true;
+			}
+
+			Tuple<int, int> rowAndCol = GetUnassignedLocations(sudokuGrid);
+			int row = rowAndCol.Item1;
+			int col = rowAndCol.Item2;
+
+			for (int num = 1; num <= 9; num++)
+			{
+				if (IsTheMoveLegal(sudokuGrid, row, col, num))
+				{
+					sudokuGrid[row, col] = num;
+					if (SolveSoduku(sudokuGrid))
+					{
+						return true;
+					}
+					sudokuGrid[row, col] = 0;
+				}
+			}
+			return false;
 		}
 	}
 }
